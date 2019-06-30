d6e739756551eb9a46ac429b4a14faf97d26d3a3
Alex Buck <alexjbuck@gmail.com>
2019-06-29 21:56:21 -0700

    resetting file

--- imageShiftBlend.py
+++ imageShiftBlend.py
@@ -4,36 +4,36 @@ import numpy as np
 import math
 from matplotlib.widgets import Slider
 
-def XYW(c,n,extra):
-	"""This function calculates values for X, Y, and W, the weights
+def XYV(c,n,extra):
+	"""This function calculates values for X, Y, and V, the weights
 	given values of c (scale), n (number of frames), extra (buffer frames)"""
 	n = int(n)
 	extra = int(extra)
 	N = np.linspace(-extra,n+extra,2*extra+n+1)
 	X = np.linspace(-extra,n+extra,256*n,endpoint=True)
 	Y = np.zeros((len(N),len(X)))
-	W = np.zeros((len(N)+1,len(X)))
+	V = np.zeros((len(N)+1,len(X)))
 
 	# Calculate Y, the boundary lines for determining weights
 	for j in range(0,len(N)):
 		Y[j] = 1/2 + np.arctan(c*(X-N[j]))/math.pi
 
-	# Calculate W, the weights
-	W[0] = np.ones(len(X))-Y[0]
+	# Calculate V, the weights
+	V[0] = np.ones(len(X))-Y[0]
 	for j in range(1,len(N)):
-		W[j] = Y[j-1]-Y[j]
-	W[len(N)] = Y[len(N)-1]-np.zeros(len(X))
+		V[j] = Y[j-1]-Y[j]
+	V[len(N)] = Y[len(N)-1]-np.zeros(len(X))
 
-	# Return X, Y, and W
-	return X,Y,W
+	# Return X, Y, and V
+	return X,Y,V
 
-def replot(fig,ax,n,X,Y,W):
+def replot(fig,ax,n,X,Y,V):
 	"""This function clears the subplot axes and then replots
 	"""
 	ax[0].clear()
 	ax[0].plot(X,Y.transpose())
 	ax[1].clear()
-	ax[1].plot(X,W.transpose())
+	ax[1].plot(X,V.transpose())
 	ax[0].set_xlim(0,n)
 	ax[1].set_xlim(0,n)
 	ax[0].set_title('Image Transitions')
@@ -48,14 +48,14 @@ def update(val):
 	c = sc.val
 	n = sn.val
 	e = se.val
-	X,Y,w = XYW(c,n,e)
-	replot(fig,ax,n,X,Y,W)
+	X,Y,V = XYV(c,n,e)
+	replot(fig,ax,n,X,Y,V)
 
 c = 5
 n = int(5)
 e = int(0)
 
-X,Y,W = XYW(c,n,e)
+X,Y,V = XYV(c,n,e)
 
 fig,ax = plt.subplots(2,constrained_layout=True)
 
@@ -74,7 +74,7 @@ sc.on_changed(update)
 sn.on_changed(update)
 se.on_changed(update)
 
-replot(fig,ax,n,X,Y,W)
+replot(fig,ax,n,X,Y,V)
 
 # plt.figure(3)
 # S = np.zeros_like(X)
