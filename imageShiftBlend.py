1e7326a37d08e9afbc8bafd46c23bddf70fd40c2
Alex Buck <alexjbuck@gmail.com>
2019-06-30 12:28:25 -0700

    Refactored code to allow for an interactive mode via the flag --interactive. This allows you to see what effect the blending options have operating on a thumbnail view of the image. Without the --interactive flag the program runs once through, creates an image with the given blend factor (-b) and saves it to file. Began to lay groundwork for alternative blending functions.

--- imageShiftBlend.py
+++ imageShiftBlend.py
@@ -22,11 +22,20 @@ def printStatus(level,text):
 		pre += "-"
 	pre += "> "
 	print(pre+text)
-
-def XYW(c,n,extra,w):
-	"""This function calculates values for X, Y, and V, the weights
-	given values of c (scale), n (number of frames), extra (buffer frames)
-	w is the width in pixels of each frame.
+def linearWeights(b,n,extra,w):
+	'''
+	Currently outputs all zero weights. Placeholder for further development
+	'''
+	N = np.linspace(-extra,n+extra,2*extra+n-1)
+	X = np.linspace(-extra,n+extra,w,endpoint=True)
+	Y = np.zeros((len(N)+1,len(X)))
+	W = np.zeros((len(N)+1,len(X)))
+	return X,Y,W
+def arctanWeights(b,n,extra,w):
+	"""
+	This function calculates values for X, Y, and V, the weights
+	given values of b (blur factor), n (number of frames), extra (buffer frames)
+	w is the width in pixels of each frame
 	"""
 	n = int(n)
 	w = int(w)
@@ -34,7 +43,7 @@ def XYW(c,n,extra,w):
 	# N linspace from 0 to n with integer spacing, length n-1
 	N = np.linspace(-extra,n+extra,2*extra+n-1)
 	# X linspace from 0 to n with w points representing the w pixels wide of the frame
-	X = np.linspace(-extra,n+extra,w,endpoint=True)
+	X = np.linspace(0,n,w,endpoint=True)
 	# Y is dimension n x w
 	Y = np.zeros((len(N)+1,len(X)))
 	# W is dimension n x w
@@ -43,7 +52,7 @@ def XYW(c,n,extra,w):
 	# Calculate Y, the boundary lines for determining weights
 	Y[0] = np.ones(len(X))
 	for j in range(0,len(N)):
-		Y[j+1] = 1/2 + np.arctan(c*(X-N[j]))/math.pi
+		Y[j+1] = 1/2 + np.arctan(b*(X-N[j]))/math.pi
 
 	# Calculate W, the weights
 	for j in range(0,len(N)):
@@ -52,36 +61,30 @@ def XYW(c,n,extra,w):
 
 	# Return X, Y, and W
 	return X,Y,W
-
-def replot(fig,ax,n,X,Y,W):
+def replot(fig,axi,axw,b,X,W,frame):
 	"""This function clears the subplot axes and then replots
 	"""
-	ax[0].clear()
-	ax[0].plot(X,Y.transpose())
-	ax[1].clear()
-	ax[1].plot(X,W.transpose())
-	ax[0].set_xlim(0,n)
-	ax[1].set_xlim(0,n)
-	ax[0].set_title('Image Transitions')
-	ax[1].set_title('Image Blending Weights')
+	axi.clear()
+	axi.imshow(frame)
+	axi.set_title('Image Blend Factor: '+str(b))
+
+	axw.clear()
+	axw.plot(X,W.transpose())
+	axw.set_title('Image Blending Weights')
 	fig.canvas.draw()
 	fig.canvas.flush_events()
-
 def update(val):
-	"""This function updates the values of c, n, and e then runs XYW
+	"""This function updates the values of b, n, and e then runs calcWeights
 	and updates the plots"""
-	global fig, ax, w
-	c = sc.val
-	n = sn.val
-	e = se.val
-	w = w
-	X,Y,W = XYW(c,n,e,w)
-	replot(fig,ax,n,X,Y,W)
-	filename="img_"+str(n)+"_"+str(c)+".jpg"
-	outImg = Image.fromarray(reweight(frames,W).astype('uint8'))
-	outImg.show()
-	outImg.save(filename)
-
+	b = round(sb.val,1)
+	# Removing n and e sliders
+	# n = sn.val
+	# e = se.val
+	X,Y,W = calcWeights(b,n,e,w)
+	frame = reweight(frames,W)
+	replot(fig,axi,axw,b,X,W,frame)
+	# filename="img_"+str(n)+"_"+str(b)+".jpg"
+	# Image.fromarray(frame).save(filename)
 def reweight(frames,W):
 	printStatus(0,"Beginning frame weighting")
 	outFrame = np.zeros_like(frames[0])
@@ -93,53 +96,101 @@ def reweight(frames,W):
 			# multiply every pixel in the column by the weight for this image (i) and this column (j)
 			# Add the scaled frame to the outFrame
 			outFrame[:,j,:]+=frames[i,:,j,:]*W[i,j]
+	# Clip values to 0-255
+	np.clip(outFrame,0,255,out=outFrame)
 	printStatus(1,"Done!")
-	return outFrame
+	return outFrame.astype('uint8')
 
+"""
+Argument Parser
+"""
 parser = argparse.ArgumentParser(description='Input N image files and blend them left to right using arctan based weighting. N-2 central frames with weighting peaks and 2 edge frames.')
-parser.add_argument('--input','-i',help="Input files.",required=True,dest='imgList',nargs='+',type=str,default=('1.jpg','2.jpg','3.jpg'))
+parser.add_argument('-i',help="Input files.",required=False,dest='imgList',nargs='+',type=str,default=('0.jpg','1.jpg','2.jpg','3.jpg','4.jpg'))
+parser.add_argument('--interactive',help="Toggles on interactive mode.",required=False,dest='interactive',action='store_true',default=False)
+parser.add_argument('-b',help='Assigns the blend factor used to determine the weights. Simply it sets the horizontal scaling of the arctan functions used to generate weights.',required=False,dest='b',nargs=1,default=2.7,type=float)
+parser.add_argument('-wf',help='Assign which weighting function to use for blending the image.',required=False,default='arctan',choices=['arctan','linear'])
+args 	= parser.parse_args()
+imgList	= args.imgList
+b 		= args.b
+interactive = args.interactive
 
-args=parser.parse_args()
-imgList=args.imgList
+# Select weighting function
+if args.wf=="arctan":
+	calcWeights = arctanWeights
+elif args.wf=="linear":
+	calcWeights	= linearWeights
+else:
+	print("Something went very wrong when parsing arguments. I'm going to give up.")
+	quit()
+
+"""
+Done Parsing Arguments
+
+"""
+
+'''
+	Begin Main Program
+'''
+img = Image.open(imgList[0]).copy()
+if 	interactive:
+	thumbsize = 720,720
+else:
+	thumbsize = img.size
 
 printStatus(0,imgList)
-
 n = int(len(imgList))
-frame = np.asarray(Image.open(imgList[0]));
+img.thumbnail(thumbsize)
+frame = np.asarray(img);
+frames = np.zeros((n,frame.shape[0],frame.shape[1],frame.shape[2]))
 h,w = frame.shape[0],frame.shape[1]
 
-frames = np.zeros((n,frame.shape[0],frame.shape[1],frame.shape[2]))
+# Begin loading the images to memory
+printStatus(0,'Loading Images...')
 for i in range(0,n):
-	frames[i] = np.asarray(Image.open(imgList[i]).copy()).astype('uint32')
-	printStatus(1,imgList[i]+" loaded.")
+	img = Image.open(imgList[i]).copy()
+	img.thumbnail(thumbsize)
+	frames[i] = np.asarray(img)
+	printStatus(1,imgList[i]+' loaded.')
+printStatus(1,'Done!')
 
-c = 2.7
+# Initial parameter values
 n = int(n)
-e = int(0)
+e = int(0) # Forcing to zero until I implment how to handle this on the image. Allowing extra > 0 means the weights for the image frame no longer start at the zero index and I haven't implemented a way to handle this yet.
 w = int(w)
-X,Y,W = XYW(c,n,e,w)
-filename="img_"+str(n)+"_"+str(c)+".jpg"
-outImg = Image.fromarray(reweight(frames,W).astype('uint8'))
-outImg.show()
-outImg.save(filename)
 
-fig,ax = plt.subplots(2,constrained_layout=True)
+if interactive:
+	fig = plt.figure(constrained_layout=True)
+	gs 	= fig.add_gridspec(10,1)
+	axi = fig.add_subplot(gs[0:5])
+	axw = fig.add_subplot(gs[6:8])
+	axb = fig.add_subplot(gs[9])
+	# fig,(axi,axw,axc) = plt.subplots(3,1)
 
-fig2 = plt.figure(2)
+	# n slider isn't needed when working with real image set, n is provided by number of images assuming extra is zero
+	# axn = plt.axes([0.15, 0.35, 0.7, 0.3], facecolor=axcolor)
+	# Removing extra feature. Only allowing extra=0 for now.
+	# axe = plt.axes([0.15, 0.65, 0.7, 0.3], facecolor=axcolor)
 
-axcolor = 'pink'
-axc = plt.axes([0.15, 0.05, 0.7, 0.3], facecolor=axcolor)
-axn = plt.axes([0.15, 0.35, 0.7, 0.3], facecolor=axcolor)
-axe = plt.axes([0.15, 0.65, 0.7, 0.3], facecolor=axcolor)
+	axw.set_ylim(0,1)
 
-sc = Slider(axc, 'c', .1, 10.0, valinit=c, valstep=.1)
-sn = Slider(axn, 'n', 1, 10, valinit=n, valstep=1)
-se = Slider(axe, 'e', 0, 10, valinit=e,valstep=1)
+	sb = Slider(axb, 'b', .1, 10.0, valinit=b, valstep=.1)
+	# sn = Slider(axn, 'n', 1, 10, valinit=n, valstep=1)
+	# se = Slider(axe, 'e', 0, 10, valinit=e,valstep=1)
 
-sc.on_changed(update)
-sn.on_changed(update)
-se.on_changed(update)
+	sb.on_changed(update)
+	# sn.on_changed(update)
+	# se.on_changed(update)
 
-replot(fig,ax,n,X,Y,W)
+	update(b)
+	# replot(fig,ax,n,X,Y,W)
 
-plt.show()
+	plt.show()
+
+else:
+	W = calcWeights(b,n,e,w)[2]
+	outFrame = reweight(frames,W)
+	filename="out_"+str(n)+"_"+str(b)+".jpg"
+	Image.fromarray(outFrame).save(filename)
+	printStatus(0,'File saved: '+filename)
+
+
