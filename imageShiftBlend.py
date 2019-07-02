c03ad876cf315e9dc9dc5ee391266f14f8ee5349
Alex Buck <alexjbuck@gmail.com>
2019-07-02 00:11:39 -0700

    Added HSV/HSL processing, attempting to preserve the hue strength of the original images in their respective field. Small improvements noted. Weighting curves need to be worked on next.

--- imageShiftBlend.py
+++ imageShiftBlend.py
@@ -1,12 +1,7 @@
 #!/usr/local/bin/python3
 from matplotlib.widgets import Slider
-from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
-import math
-import argparse
-
-import time
+from PIL import Image, ImageMath
+import colorsys, argparse, math, time, numpy as np, matplotlib.pyplot as plt
 
 t0 = time.time()
 
@@ -22,32 +17,36 @@ def printStatus(level,text):
 		pre += "-"
 	pre += "> "
 	print(pre+text)
-def linearWeights(b,n,extra,w):
+def linearWeights(b,n,e,w):
 	'''
 	Currently outputs all zero weights. Placeholder for further development
 	'''
-	N = np.linspace(-extra,n+extra,2*extra+n-1)
-	X = np.linspace(-extra,n+extra,w,endpoint=True)
+	N = np.linspace(-e,n+e,2*e+n-1)
+	X = np.linspace(-e,n+e,w,endpoint=True)
 	Y = np.zeros((len(N)+1,len(X)))
 	W = np.zeros((len(N)+1,len(X)))
 	return X,Y,W
-def arctanWeights(b,n,extra,w):
+def logisticWeights(b,n,e,w):
+	'''
+	Currently outputs all zero weights. Placeholder for further development
+	'''
+	N = np.linspace(-e,n+e,2*e+n-1)
+	X = np.linspace(-e,n+e,w,endpoint=True)
+	Y = np.zeros((len(N)+1,len(X)))
+	W = np.zeros((len(N)+1,len(X)))
+def arctanWeights(b,n,e,w):
 	"""
 	This function calculates values for X, Y, and V, the weights
-	given values of b (blur factor), n (number of frames), extra (buffer frames)
+	given values of b (blur factor), n (number of frames), e (buffer frames)
 	w is the width in pixels of each frame
 	"""
 	n = int(n)
 	w = int(w)
-	extra = int(extra)
-	# N linspace from 0 to n with integer spacing, length n-1
-	N = np.linspace(-extra,n+extra,2*extra+n-1)
-	# X linspace from 0 to n with w points representing the w pixels wide of the frame
-	X = np.linspace(0,n,w,endpoint=True)
-	# Y is dimension n x w
-	Y = np.zeros((len(N)+1,len(X)))
-	# W is dimension n x w
-	W = np.zeros((len(N)+1,len(X)))
+	e = int(e)
+	N = np.linspace(-e,n+e,2*e+n-1)	# N linspace from 0 to n with integer spacing, length n-1
+	X = np.linspace(0,n,w,endpoint=True)	# X linspace from 0 to n with w points representing the w pixels wide of the frame
+	Y = np.zeros((len(N)+1,len(X)))	# Y is dimension n x w
+	W = np.zeros((len(N)+1,len(X)))	# W is dimension n x w
 
 	# Calculate Y, the boundary lines for determining weights
 	Y[0] = np.ones(len(X))
@@ -59,15 +58,14 @@ def arctanWeights(b,n,extra,w):
 		W[j] = Y[j]-Y[j+1]
 	W[len(N)] = Y[len(N)]-np.zeros(len(X))
 
-	# Return X, Y, and W
+	# Return X, Y, and W (X and Y for plotting)
 	return X,Y,W
 def replot(fig,axi,axw,b,X,W,frame):
 	"""This function clears the subplot axes and then replots
 	"""
 	axi.clear()
-	axi.imshow(frame)
-	axi.set_title('Image Blend Factor: '+str(b))
-
+	axi.imshow(frameCMtoRGB(frame))
+	axi.set_title(args.cm + ' - Image Blend Factor: '+str(b))
 	axw.clear()
 	axw.plot(X,W.transpose())
 	axw.set_title('Image Blending Weights')
@@ -77,14 +75,9 @@ def update(val):
 	"""This function updates the values of b, n, and e then runs calcWeights
 	and updates the plots"""
 	b = round(sb.val,1)
-	# Removing n and e sliders
-	# n = sn.val
-	# e = se.val
 	X,Y,W = calcWeights(b,n,e,w)
 	frame = reweight(frames,W)
 	replot(fig,axi,axw,b,X,W,frame)
-	# filename="img_"+str(n)+"_"+str(b)+".jpg"
-	# Image.fromarray(frame).save(filename)
 def reweight(frames,W):
 	printStatus(0,"Beginning frame weighting")
 	outFrame = np.zeros_like(frames[0])
@@ -96,101 +89,174 @@ def reweight(frames,W):
 			# multiply every pixel in the column by the weight for this image (i) and this column (j)
 			# Add the scaled frame to the outFrame
 			outFrame[:,j,:]+=frames[i,:,j,:]*W[i,j]
-	# Clip values to 0-255
-	np.clip(outFrame,0,255,out=outFrame)
 	printStatus(1,"Done!")
-	return outFrame.astype('uint8')
+	return outFrame
+
+def frameRGBtoRGB(frame):
+	return frame.astype('uint8')
+def frameRGBtoHSV(frame):
+	"""	NOTE: VALUES ACTUALLY STORED ARE CARTESIAN XYZ COORDINATES FOR THE HSV CYLINDRICAL COORDINATES. THIS IS DONE TO SUPPORT AVERAGING. SINCE I NEVER NEED TO ACCESS THE PIXEL VALUES AT THIS POINT FOR ANYTHING ELSE.
+	"""	
+	frame = frame.astype('float')
+	for i in range(frame.shape[0]):
+		for j in range(frame.shape[1]):
+			r,g,b = frame[i,j]
+			r /= 255.0
+			g /= 255.0
+			b /= 255.0
+			h,s,v = colorsys.rgb_to_hsv(r,g,b)
+			x = s*math.cos(h*2*math.pi)
+			y = s*math.sin(h*2*math.pi)
+			frame[i,j] = (x,y,v)
+	return frame
+def frameHSVtoRGB(frame):
+	"""	NOTE: VALUES ACTUALLY STORED ARE CARTESIAN XYZ COORDINATES FOR THE HSV CYLINDRICAL COORDINATES. THIS IS DONE TO SUPPORT AVERAGING. SINCE I NEVER NEED TO ACCESS THE PIXEL VALUES AT THIS POINT FOR ANYTHING ELSE.
+	"""	
+	for i in range(frame.shape[0]):
+		for j in range(frame.shape[1]):
+			x,y,v = frame[i,j]
+			h,s = (math.atan2(y,x)/(2*math.pi),math.sqrt(x**2+y**2))
+			h %= 1
+			s = min(s,1)
+			r,g,b = colorsys.hsv_to_rgb(h,s,v)
+			r *= 255
+			g *= 255
+			b *= 255
+			frame[i,j] = (r,g,b)
+	np.clip(frame,0,255,out=frame)
+	return frame.astype('uint8')
+def frameRGBtoHSL(frame):
+	"""	NOTE: VALUES ACTUALLY STORED ARE CARTESIAN XYZ COORDINATES FOR THE HSL CYLINDRICAL COORDINATES. THIS IS DONE TO SUPPORT AVERAGING. SINCE I NEVER NEED TO ACCESS THE PIXEL VALUES AT THIS POINT FOR ANYTHING ELSE.
+	"""	
+	frame = frame.astype('float')
+	for i in range(frame.shape[0]):
+		for j in range(frame.shape[1]):
+			r,g,b = frame[i,j]
+			r /= 255.0
+			g /= 255.0
+			b /= 255.0
+			h,l,s = colorsys.rgb_to_hls(r,g,b)
+			x = s*math.cos(h*2*math.pi)
+			y = s*math.sin(h*2*math.pi)
+			frame[i,j] = (x,y,l)
+	return frame
+def frameHSLtoRGB(frame):
+	"""	NOTE: VALUES ACTUALLY STORED ARE CARTESIAN XYZ COORDINATES FOR THE HSL CYLINDRICAL COORDINATES. THIS IS DONE TO SUPPORT AVERAGING. SINCE I NEVER NEED TO ACCESS THE PIXEL VALUES AT THIS POINT FOR ANYTHING ELSE.
+	"""	
+	for i in range(frame.shape[0]):
+		for j in range(frame.shape[1]):
+			x,y,l = frame[i,j]
+			h,s = (math.atan2(y,x)/(2*math.pi),math.sqrt(x**2+y**2))
+			h %= 1
+			s = min(s,1)
+			r,g,b = colorsys.hls_to_rgb(h,l,s)
+			r *= 255
+			g *= 255
+			b *= 255
+			frame[i,j] = (r,g,b)
+	np.clip(frame,0,255,out=frame)
+	return frame.astype('uint8')
+def horizImageMask(size,W): # THIS IS FUTURE WORK AREA, MAY NOT PURSUE
+	return np.broadcast_to(W,size)
+def maskImage(frame,mask): # THIS IS FUTURE WORK AREA, MAY NOT PURSUE
+	return ImageMath.eval("a*b",a=frame,b=mask)
 
 """
-Argument Parser
+	Argument Parser
 """
 parser = argparse.ArgumentParser(description='Input N image files and blend them left to right using arctan based weighting. N-2 central frames with weighting peaks and 2 edge frames.')
 parser.add_argument('-i',help="Input files.",required=False,dest='imgList',nargs='+',type=str,default=('0.jpg','1.jpg','2.jpg','3.jpg','4.jpg'))
 parser.add_argument('--interactive',help="Toggles on interactive mode.",required=False,dest='interactive',action='store_true',default=False)
+parser.add_argument('--thumbsize',help="Sets bounding box for thumbnail image size.",nargs=2,type=int,required=False,default=(480,480))
+parser.add_argument('--outsize',help="Sets bounding box for output image size.",nargs=2,type=int,required=False,default=None)
 parser.add_argument('-b',help='Assigns the blend factor used to determine the weights. Simply it sets the horizontal scaling of the arctan functions used to generate weights.',required=False,dest='b',nargs=1,default=2.7,type=float)
-parser.add_argument('-wf',help='Assign which weighting function to use for blending the image.',required=False,default='arctan',choices=['arctan','linear'])
-args 	= parser.parse_args()
-imgList	= args.imgList
-b 		= args.b
-interactive = args.interactive
+parser.add_argument('-wf',help='Assign which weighting function to use for blending the image.',required=False,default='arctan',choices=['arctan','linear','logistic'])
+parser.add_argument('-cm',help='Assign which color model to use when blending images.',required=False,default='hsl',choices=['rgb','hsl','hsv'])
 
+args 		= parser.parse_args()
+imgList		= args.imgList
+b 			= args.b
+interactive = args.interactive
+outsize 	= args.outsize
+"""
+	Done Parsing Arguments
+"""
+
+"""
+	Begin Main Program
+"""
 # Select weighting function
 if args.wf=="arctan":
 	calcWeights = arctanWeights
 elif args.wf=="linear":
 	calcWeights	= linearWeights
+elif args.wf=="logistic":
+	calcWeights	= logisticWeights
 else:
-	print("Something went very wrong when parsing arguments. I'm going to give up.")
+	print("Something went very wrong when parsing weighting function arguments. I'm going to give up.")
 	quit()
 
-"""
-Done Parsing Arguments
-
-"""
-
-'''
-	Begin Main Program
-'''
-img = Image.open(imgList[0]).copy()
-if 	interactive:
-	thumbsize = 720,720
+# Select color model conversion function
+if args.cm=="rgb":
+	frameRGBtoCM = frameRGBtoRGB
+	frameCMtoRGB = frameRGBtoRGB
+elif args.cm=="hsl":
+	frameRGBtoCM = frameRGBtoHSL
+	frameCMtoRGB = frameHSLtoRGB
+elif args.cm=="hsv":
+	frameRGBtoCM = frameRGBtoHSV
+	frameCMtoRGB = frameHSVtoRGB
 else:
-	thumbsize = img.size
+	print("Something went very wrong when parsing color model arguments. I'm going to give up.")
+	quit()
 
-printStatus(0,imgList)
-n = int(len(imgList))
+# Open first image, resize as needed
+img = Image.open(imgList[0])
+if 	interactive:
+	thumbsize = args.thumbsize
+else:
+	if outsize==None:
+		thumbsize = img.size
+	else:
+		thumbsize = outsize
+
 img.thumbnail(thumbsize)
-frame = np.asarray(img);
-frames = np.zeros((n,frame.shape[0],frame.shape[1],frame.shape[2]))
-h,w = frame.shape[0],frame.shape[1]
 
-# Begin loading the images to memory
-printStatus(0,'Loading Images...')
+# Pull image dimensions from first loaded image, initialize frames ndarray
+n 		= int(len(imgList))
+frame  	= np.asarray(img);
+h,w 	= frame.shape[0:2]
+frames 	= np.zeros((n,h,w,frame.shape[2]))
+
+# Begin loading the images to memory in 'frames'
+printStatus(0,'Loading Images: '+ str(imgList))
 for i in range(0,n):
-	img = Image.open(imgList[i]).copy()
+	img = Image.open(imgList[i])
 	img.thumbnail(thumbsize)
-	frames[i] = np.asarray(img)
+	frames[i] = frameRGBtoCM(np.asarray(img))
 	printStatus(1,imgList[i]+' loaded.')
 printStatus(1,'Done!')
 
-# Initial parameter values
-n = int(n)
+# Fixing parameter values as required
 e = int(0) # Forcing to zero until I implment how to handle this on the image. Allowing extra > 0 means the weights for the image frame no longer start at the zero index and I haven't implemented a way to handle this yet.
-w = int(w)
 
 if interactive:
-	fig = plt.figure(constrained_layout=True)
+	fig = plt.figure()
 	gs 	= fig.add_gridspec(10,1)
-	axi = fig.add_subplot(gs[0:5])
-	axw = fig.add_subplot(gs[6:8])
+	axi = fig.add_subplot(gs[0:6])
+	axw = fig.add_subplot(gs[6:9])
 	axb = fig.add_subplot(gs[9])
-	# fig,(axi,axw,axc) = plt.subplots(3,1)
-
-	# n slider isn't needed when working with real image set, n is provided by number of images assuming extra is zero
-	# axn = plt.axes([0.15, 0.35, 0.7, 0.3], facecolor=axcolor)
-	# Removing extra feature. Only allowing extra=0 for now.
-	# axe = plt.axes([0.15, 0.65, 0.7, 0.3], facecolor=axcolor)
-
 	axw.set_ylim(0,1)
 
 	sb = Slider(axb, 'b', .1, 10.0, valinit=b, valstep=.1)
-	# sn = Slider(axn, 'n', 1, 10, valinit=n, valstep=1)
-	# se = Slider(axe, 'e', 0, 10, valinit=e,valstep=1)
-
 	sb.on_changed(update)
-	# sn.on_changed(update)
-	# se.on_changed(update)
-
 	update(b)
-	# replot(fig,ax,n,X,Y,W)
-
 	plt.show()
-
 else:
 	W = calcWeights(b,n,e,w)[2]
-	outFrame = reweight(frames,W)
-	filename="out_"+str(n)+"_"+str(b)+".jpg"
-	Image.fromarray(outFrame).save(filename)
+	outFrame = frameCMtoRGB(reweight(frames,W))
+	filename = args.cm+str(n)+"_"+str(b)+".jpg"
+	outImg = Image.fromarray(outFrame).save(filename)
 	printStatus(0,'File saved: '+filename)
 
 
